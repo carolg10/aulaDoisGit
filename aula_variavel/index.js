3217affbacd847c55a4a8dcae0b5eb638c9f4326
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 11:52:40 -0300

    testando conversão string/number

--- aula_variavel/index.js
+++ aula_variavel/index.js
@@ -1,5 +1,10 @@
-console.log("Hello world")
+console.log("Hello world") // console.log serve para imprimir uma mensagem!
 
-const nome = "Carol" 
+const nome = "Carol" // const = constancia 
 const idade = 18 
-console.log("Meu nome é", nome, "tenho", idade, "anos")
+console.log("Meu nome é", nome, "tenho", idade, "anos") // mensagem + variavel
+// teste
+const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)

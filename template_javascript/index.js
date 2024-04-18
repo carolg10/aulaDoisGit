a04821f68e4213f7f8d84045d996903bbcf40e5d
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-18 09:58:42 -0300

    let, alert, prompt, &&, ||

--- template_javascript/index.js
+++ template_javascript/index.js
@@ -1,23 +1,4 @@
-let nome = prompt("digite seu nome: ")
-let idade = prompt("digite sua idade: ")
-// MENSAGEM: Digite as horas que você trabalha por dia:
-let horasTrabalho = prompt("digite as horas que você trabalha: ")
-let salarioDia = prompt("digite seu salário diário")
-// Verificando as variaveis
-console.log(typeof nome)
-console.log(typeof horasTrabalho)
-console.log(typeof salarioDia)
-// Convertendo o que é necessario 
-let horasTrabNumber = Number(horasTrabalho)
-let salDiaNumber = Number(salarioDia)
-// Processando valores
-let valHoraNumber = horasTrabNumber/salDiaNumber
-// Conversão para não dar erro de NULL
-let valorHora = String(valHoraNumber)
-const mensagem = ("seu valor é ") + valorHora + "!!!"
-// Verificando variaveis
-console.log(typeof valorHora)
-console.log(typeof mensagem)
-console.log(mensagem)
-//saida de valores para Front End
-alert(mensagem)
+const primeiroNumero = 3
+const segundoNumero = 4
+const resultado = primeiroNumero + segundoNumero
+console.log(resultado)

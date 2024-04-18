4a4c7278d3db5288a680ff1bf88a1687a0fecb31
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-18 12:01:48 -0300

    string e arrays

---
+++ aula_1804/index.js
@@ -0,0 +1,15 @@
+/* let nome = prompt ("Qual seu nome? ")
+let cor = prompt("Qual sua cor favorita? ")
+//alert("A cor favorita de" + nome "é" + cor)
+alert(`cor favorita de ${nome} é ${cor}`)
+// print cor favorita de carol é  rosa */
+ 
+let frase = prompt("Digite uma frase: ")
+console.log(frase.toUpperCase())
+console.log(frase.replacell("o","i"))
+console.log(frase.length)
+console.log(frase.replacell("o","i").toUpperCase().replacell("H","P"))
+
+/*let racaCachorro = ("Pittbull", "caneCorso", "ViraLata")
+let optionRaca = prompt("Digite um num de 0 a 3")
+alert(racaCachorro{optionRaca})*/

259d1dd9b79ceb9ce5d3b57c5a9b85b98b4843e7
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-22 11:28:52 -0300

    exercicios string/arrays

--- aula string e arrays/index.js
+++ aula string e arrays/index.js
@@ -10,20 +10,12 @@ console.log(frase.replacell("o","i"))
 console.log(frase.length)
 console.log(frase.replacell("o","i").toUpperCase().replacell("H","P"))*/
 
-/*let racaCachorro = ("pincher", "poodle", "salsicha mofada" ,"caramelo", "bulldog")
-let optionRaca = prompt("Digite um num de 1 a 4")
-alert(racaCachorro{optionRaca})*/
-
 /*let number + [1,2,,4,5,6]
 console.log(numbers.length)
 numbers.push(7)
 numbers.splice(3,2)
 console.log(numbers.length)
 console.log(numbers)*/
-
-/*let nome = prompt("Digite seu nome")
-let corFav = prompt("Digite sua cor favorita")
-alert("A cor favorita de " + nome + " é " + corFav)*/
 
 /*let racaDeCachorro
 racaDeCachorro = ["Pinscher", "Pitbull", "Caramelo", "ChowChow", "Salsicha Mofada"]
@@ -36,8 +28,24 @@ nomeDoUsuario = prompt("Digite seu nome: ")
 emailDoUsuario = prompt("Digite seu email: ")
 alert(`O email ${emailDoUsuario} foi cadrastrado com sucesso! Seja bem-vindo ${nomeDoUsuario}`)*/
 
-let comidasFavs = ["Lentilha", "Lasanha", "Batata frita", "Pão de batata", "Brocolis"]
+/*let comidasFavs = ["Lentilha", "Lasanha", "Batata frita", "Pão de batata", "Brocolis"]
 console.log(comidasFavs[0] + "\n" + "\n" + comidasFavs[1] + "\n" + comidasFavs[2] + "\n" + comidasFavs[3] + "\n" + comidasFavs[4])
 alert("Essas são minhas comidas favoritas: " + "\n" + comidasFavs[0] + "\n" + comidasFavs[1] + "\n" + comidasFavs[2] + "\n" + comidasFavs[3] + "\n" + comidasFavs[4])
 const comidaUsuario = prompt("Qual sua comida favorita?")
-alert("Essas são minhas comidas favoritas: " + "\n" + comidaUsuario + "\n" + comidasFavs[1] + "\n" + comidasFavs[2] + "\n" + comidasFavs[3] + "\n" + comidasFavs[4])
+alert("Essas são minhas comidas favoritas: " + "\n" + comidaUsuario + "\n" + comidasFavs[1] + "\n" + comidasFavs[2] + "\n" + comidasFavs[3] + "\n" + comidasFavs[4])*/
+
+let array
+array = []
+let listaDeTarefas = array
+let tarefasUsuario = prompt("Digite uma tarefa que você precise realizar no dia de hoje: ")
+    listaDeTarefas.push(tarefasUsuario)
+    console.log(listaDeTarefas)
+    
+    tarefasUsuario = prompt("Digite uma tarefa que você precise realizar no dia de hoje: ")
+    listaDeTarefas.push(tarefasUsuario)
+    console.log(listaDeTarefas)
+    
+    tarefasUsuario = prompt("Digite uma tarefa que você precise realizar no dia de hoje: ")
+    listaDeTarefas.push(tarefasUsuario)
+    console.log(listaDeTarefas)
+let indiceUsuario = prompt("Digite o índice de uma tarefa que já realizou: ")
--- template_javascript/index.js
+++
@@ -1,4 +0,0 @@
-const primeiroNumero = 3
-const segundoNumero = 4
-const resultado = primeiroNumero + segundoNumero
-console.log(resultado)

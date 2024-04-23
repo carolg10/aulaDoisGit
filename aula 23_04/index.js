e71a24e0cbb63b5d3ea5e454e5a7db83909acab6
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-23 11:58:31 -0300

    Exercicios condicionais

---
+++ aula 23_04/index.js
@@ -0,0 +1,54 @@
+//xercicio 1 e 2
+/*let num1 = prompt("Digite um número")
+let num2 = prompt("Digite outro número")
+if (num1 === num2){
+    console.log("Esses números são iguais")
+} else {
+    console.log("Números diferentes")
+}*/
+ 
+//exercicio 3
+/*let num1 = prompt("Digite o primiro número")
+let num2 = prompt("Digite o segundo número")
+if(num1 === num2){
+    alert(`Números iguais`)
+}else if (num1 > num2){
+    alert(`${num1} é maior que ${num2}`)
+}else if (num1 < num2){
+    alert(`${num1} é menor que ${num2}`)
+}*/
+
+//exercicio 4
+/*let tipoDePokemon = prompt("Digite um pokemon")
+switch(tipoDePokemon){
+    case 'Bulbasauro':
+        alert("Planta e veneno")
+        break
+    case 'Charmander':
+        alert("Fogo")
+        break
+    case 'Squirtle':
+        alert("Agua")
+        break
+    default:
+        alert("Pokemon não encontrado")
+        break
+}*/
+
+//exercicio 5
+/*let idade = Number(prompt("Qual sua idade?"))
+let ensinoMedio = prompt("Concluiu o ensino médio? sim - não")
+let faculdade = prompt("Esta na faculdade? sim - não")
+if ((idade >= 18) && (ensinoMedio == "sim") && (faculdade == "não")){
+    alert("Voce esta apto para entrar na faculdade")
+} else{
+    alert("Você não esta apto para entrar na faculdade")
+}*/
+
+// exercicios notion de escrita de código:
+let numero = Number(prompt("Digite um número."))
+if (numero >= 18){
+    alert("Voce pode dirigir")
+}else{
+    alert("Voce não pode dirigir")
+}

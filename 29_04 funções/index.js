cfd34d281723d8cd2bd4269336c55238b0878929
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-29 11:54:53 -0300

    exercicios funções

---
+++ 29_04 funções/index.js
@@ -0,0 +1,11 @@
+/*function imprimirOlaMundo() {
+    console.log("Olá mundo!")
+}
+imprimirOlaMundo()*/
+
+function imprimeNome(nome){
+    alert(`Olá ${nome}`)
+}
+let nomeUser = ""
+nomeUser = prompt("Digite seu nome")
+imprimeNome(nomeUser)
